9a756811d891ae3081dbb7d444927608f4c4844b
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-06-22 15:20:22 -0300

    add page create room, authentication firebase with google

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,12 @@
-// type ButtonProps = {
-//   //text?: string[]; // tipagem de array de string
-//   //text?: string; // tipagem de string e o ? significa nao obrigatorio receber uma props
-//   children?: string
-// }
+import { ButtonHTMLAttributes } from 'react';
 
-import { useState } from "react"
+import '../styles/button.scss'
 
-// export function Button(props: ButtonProps) {
-export function Button() {
-  const [counter, setCounter] = useState(0)
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-  function increment() {
-    setCounter(counter + 1)
-  }
+export function Button(props: ButtonProps) {
 
   return (
-    <div>
-      {/* <button>{props.text || 'Button Default'}</button> */}
-      <button onClick={increment}> {counter} </button> 
-    </div>
+    <button  className="button" {...props} />
   )
 }
